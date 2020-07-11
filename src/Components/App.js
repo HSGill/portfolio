0bde09fb7c46f7ff01cddb781a592d50a1251f4e
unknown <harjotgill73@gmail.com>
2020-07-12 09:17:19 +1200

    layout fixed right panel

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import Cover from './Cover';
-import Home  from './Home';
+import Home from './Home';
 import Projects from './Projects';
 import Particles from 'react-particles-js';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import image from "../images/HG-logo.png";
 import cv from "../images/cv.png";
-
 import github from "../images/Octocat.png";
-
 import linkedin from "../images/LI-In-Bug.png";
-
 import message from "../images/message.png";
 import profile from "../images/profile.png";
-
-
-
+import projects from "../images/projects.png";
+import opensource from "../images/open-source.png";
 //import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav.js';
@@ -32,60 +28,78 @@ const particlesOptions = {
     },
     "size": {
       "value": 2
-  }
-    
+    }
   }
 }
 
 class App extends React.Component {
   constructor(props) {
     super();
-    this.state ={
-      route:'home'
+    this.state = {
+      route: 'home'
     }
   }
-
   render() {
     return (
       <div className="container">
-      {/* <Particles className='particles' params={particlesOptions}></Particles> 
+        {/* <Particles className='particles' params={particlesOptions}></Particles> 
         <Nav></Nav>      
         <Cover> </Cover> 
         <Footer ></Footer>*/}
         {/*Left Panel */}
-   <div className="left">
-   <img className="logo" src={image} alt="portfolio" ></img>  
-   <div className="intro">
-   <p>Hi, I'm <span>Harry</span></p>
-  
-   <img className="profile" src={profile} alt="portfolio" ></img>  
+        <div className="left">
+          <img className="logo" src={image} alt="portfolio" ></img>
+          <div className="intro">
+            <p>Hi, I'm <span>Harry</span></p>
 
-   <div className="links">
-<a href="https://github.com/HSGill/" target="_blank">
+            <img className="profile" src={profile} alt="portfolio" ></img>
 
-   <img className="" src={github} alt="portfolio" ></img>  
-</a>
-<a href="https://www.linkedin.com/in/harjotgill73/"target="_blank">
+            <div className="links">
+              <a href="https://github.com/HSGill/" target="_blank">
 
-   <img className="" src={linkedin} alt="portfolio" ></img>  
-</a>
-<a href="" target="_blank">
+                <img className="" src={github} alt="portfolio" ></img>
+              </a>
+              <a href="https://www.linkedin.com/in/harjotgill73/" target="_blank">
 
-   <img className="" src={message} alt="portfolio" ></img>  
-</a>
-   <a href="" target="_blank">
-  
-   <img className="" src={cv} alt="portfolio" ></img>
-</a>
-   </div>  
-   </div> 
-   </div>
-  
-   <div className="right">
-   <div className="right-top"><h2>About Me</h2></div>
-   <div className="right-bottom"><h2>Projects</h2></div>
-       </div> 
-   </div>
+                <img className="" src={linkedin} alt="portfolio" ></img>
+              </a>
+              <a href="" target="_blank">
+
+                <img className="" src={message} alt="portfolio" ></img>
+              </a>
+              <a href="" target="_blank">
+
+                <img className="" src={cv} alt="portfolio" ></img>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="right-top">
+            <div id="about">
+              <p >About Me</p>
+              <div id="about-me">
+                <div>      I am having experience as full stack
+                development.I've worked
+</div><div>
+                  on multiple projects, and has experience with
+                  end to end
+</div>
+                <div> development and deployment of web applications.
+</div>
+              </div>
+            </div>
+
+<div className="projects">
+  <img src={projects} alt="projects"></img>
+  <img src={opensource} alt="opensource"></img>
+  <img src={projects} alt="projects"></img>
+</div>
+          </div>
+          <div className="right-bottom"><h2>Projects</h2></div>
+        </div>
+      </div>
     );
   }
 }

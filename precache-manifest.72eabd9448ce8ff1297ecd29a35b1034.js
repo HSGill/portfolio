1a7436cddd17a8ddad5c43afc510b2a1b62a6d24
HSGill <harjotgill73@gmail.com>
2020-08-11 19:54:25 +1200

    Updates

---
+++ precache-manifest.72eabd9448ce8ff1297ecd29a35b1034.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a9986ff0cddfe7186d2c39ff2b00fe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e5fe07adda56a820e8a1",
+    "url": "/portfolio/static/css/main.c77da519.chunk.css"
+  },
+  {
+    "revision": "9469f32e1fd0be0c6ffe",
+    "url": "/portfolio/static/js/2.5232e042.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.5232e042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fe07adda56a820e8a1",
+    "url": "/portfolio/static/js/main.866dde0a.chunk.js"
+  },
+  {
+    "revision": "24462be4ff6dd8ca3b28",
+    "url": "/portfolio/static/js/runtime-main.f0ff35d1.js"
+  },
+  {
+    "revision": "b847c0c509a677f2c381620b8ed31b7e",
+    "url": "/portfolio/static/media/HG-logo.b847c0c5.png"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/portfolio/static/media/Octocat.ef468e27.png"
+  },
+  {
+    "revision": "56ec4ee72a91e8f3d954db6cccbf8ab2",
+    "url": "/portfolio/static/media/cabin.56ec4ee7.png"
+  },
+  {
+    "revision": "6d3418fe72fed6f3b9e1bd0a2f015161",
+    "url": "/portfolio/static/media/cv.6d3418fe.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "/portfolio/static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "957c1c6892d9b61cfdc192b45a1bad4f",
+    "url": "/portfolio/static/media/open-source.957c1c68.png"
+  },
+  {
+    "revision": "55a31366f919a0ab595107822a921b0c",
+    "url": "/portfolio/static/media/profile.55a31366.png"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "/portfolio/static/media/safe.7dfdcf55.png"
+  }
+]);
